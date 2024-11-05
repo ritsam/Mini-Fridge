d4c6010ce241deaf582643b343ea8de1e8bbc9eb
kvtalati <kvtalati06@gmail.com>
2024-11-05 00:32:26 -0500

    stepzen api and search querey

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,72 +1,156 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+// import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/src/components/HelloWave';
-import ParallaxScrollView from '@/src/components/ParallaxScrollView';
-import { ThemedText } from '@/src/components/ThemedText';
-import { ThemedView } from '@/src/components/ThemedView';
+// import { HelloWave } from '@/src/components/HelloWave';
+// import ParallaxScrollView from '@/src/components/ParallaxScrollView';
+// import { ThemedText } from '@/src/components/ThemedText';
+// import { ThemedView } from '@/src/components/ThemedView';
+// import { Link } from 'expo-router';
+
+// export default function HomeScreen() {
+//   return (
+//     <ParallaxScrollView
+//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+//       headerImage={
+//         <Image
+//           source={require('@/src/assets/images/partial-react-logo.png')}
+//           style={styles.reactLogo}
+//         />
+//       }>
+//       <ThemedView style={styles.titleContainer}>
+//         <ThemedText type="title">Welcome!</ThemedText>
+//         <HelloWave />
+//       </ThemedView>
+//       <ThemedView style={styles.stepContainer}>
+//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+//         <ThemedText>
+//           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+//           Press{' '}
+//           <ThemedText type="defaultSemiBold">
+//             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
+//           </ThemedText>{' '}
+//           to open developer tools.
+//         </ThemedText>
+//       </ThemedView>
+//       <ThemedView style={styles.stepContainer}>
+//         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+//         <ThemedText>
+//           Tap the Explore tab to learn more about what's included in this starter app.
+//         </ThemedText>
+//       </ThemedView>
+//       <ThemedView style={styles.stepContainer}>
+//         <ThemedText type="subtitle">Step 3: hi fresh start</ThemedText>
+//         <Link href="/explore">TRIAL THIS</Link>
+//         <ThemedText>
+//           When you're ready, run{' '}
+//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+//           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+//           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+//         </ThemedText>
+//       </ThemedView>
+//     </ParallaxScrollView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   titleContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     gap: 8,
+//   },
+//   stepContainer: {
+//     gap: 8,
+//     marginBottom: 8,
+//   },
+//   reactLogo: {
+//     height: 178,
+//     width: 290,
+//     bottom: 0,
+//     left: 0,
+//     position: 'absolute',
+//   },
+// });
 import { Link } from 'expo-router';
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import { gql, useQuery } from '@apollo/client';
+import dayjs from 'dayjs';
+import FoodLogListItem from '../components/FoodLogListItem';
+
+const query = gql`
+  query foodLogsForDate($date: Date!, $user_id: String!) {
+    foodLogsForDate(date: $date, user_id: $user_id) {
+      food_id
+      user_id
+      created_at
+      label
+      kcal
+      id
+    }
+  }
+`;
 
 export default function HomeScreen() {
+  const user_id = 'vadim';
+  const { data, loading, error } = useQuery(query, {
+    variables: {
+      date: dayjs().format('YYYY-MM-DD'),
+      user_id,
+    },
+  });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch data</Text>;
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/src/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: hi fresh start</ThemedText>
-        <Link href="/explore">TRIAL THIS</Link>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.headerRow}>
+        <Text style={styles.subtitle}>Calories</Text>
+        <Text> 1770 - 360 = 1692</Text>
+      </View>
+
+      <View style={styles.headerRow}>
+        <Text style={styles.subtitle}>Today's food</Text>
+        <Link href="/search" asChild>
+          <Button title="ADD FOOD" />
+        </Link>
+      </View>
+      <FlatList
+        data={data.foodLogsForDate}
+        contentContainerStyle={{ gap: 5 }}
+        renderItem={({ item }) => <FoodLogListItem item={item} />}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10,
+    gap: 10,
+  },
+  headerRow: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  subtitle: {
+    fontSize: 18,
+    fontWeight: '500',
+    flex: 1,
+    color: 'dimgray',
   },
 });
